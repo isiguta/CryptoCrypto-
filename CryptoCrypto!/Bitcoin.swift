0b0c460c29df966a7ddf5274ea362ef47843c3f1
Igor <segamanable@gmail.com>
2018-09-21 23:54:47 -0500

    Initial commit.
    Image assets, UI stubs, model, VC.

---
+++ CryptoCrypto!/Bitcoin.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  BitcoinTicker
+//
+//  Created by Ihor Sihuta on 9/20/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Bitcoin {
+    
+    var last = 0.00
+    var low = 0.00
+    var high = 0.00
+}
--- CryptoCrypto!/ViewController.swift
+++ CryptoCrypto!/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySigns = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var selectedCurrencyRow = 0
+    var btc = Bitcoin()
+    
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
 }
-
