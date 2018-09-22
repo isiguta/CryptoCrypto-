d8148a2ace8f6cd1c045f5fd604a0ef11a7ea77a
Igor <segamanable@gmail.com>
2018-09-21 23:43:19 -0500

    Initial Commit

---
+++ CryptoCrypto!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCrypto!
+//
+//  Created by Ihor Sihuta on 9/21/18.
+//  Copyright © 2018 hustlehard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
